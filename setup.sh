561cd61b304281f313510ddb0c0644a59662074c
Andre Arko <andre@arko.net>
2016-10-11 19:45:22 -0700

    move setup into setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
+set -vx
+
+# THIS WILL DESTROY YOUR LOCAL GEMSTASH BE CAREFUL IF YOU HAVE ONE
+
+gem install gemstash
+gemstash stop || true
+rm -rf ~/.gemstash
+mkdir -p ~/.gemstash
+tar xfvz gemstash.tgz -C ~/.gemstash
+gemstash start
+
+gem install bundler
+bundle -v
