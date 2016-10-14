bc1f3e649cbfd5709e87994cba1bb09da0906d4d
Andre Arko <andre@arko.net>
2016-10-14 10:46:44 -0700

    also remove installed gems :grimace:

--- dev_repro.sh
+++ dev_repro.sh
@@ -2,6 +2,7 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 git reset --hard HEAD
+git clean -fd
 
 if [[ -f "$HOME/src/bundler/bundler/exe/bundle" ]]; then
     GEM_HOME=gems BUNDLE_DISABLE_POSTIT=1 ruby -I ~/src/bundler/bundler/lib ~/src/bundler/bundler/exe/bundle "$@";
