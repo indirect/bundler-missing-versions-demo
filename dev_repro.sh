0bbd6aa605a9bb4385cbbcfc475741229d66885a
Andre Arko <andre@arko.net>
2016-10-14 10:42:11 -0700

    add dev repro script

---
+++ dev_repro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+
+git reset --hard HEAD
+
+if [[ -f "$HOME/src/bundler/bundler/exe/bundle" ]]; then
+    GEM_HOME=gems BUNDLE_DISABLE_POSTIT=1 ruby -I ~/src/bundler/bundler/lib ~/src/bundler/bundler/exe/bundle "$@";
+else
+    GEM_HOME=gems ruby -I ~/src/bundler/bundler/lib ~/src/bundler/bundler/bin/bundle "$@";
+fi
